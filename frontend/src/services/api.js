4040dcfff6ac0738ade38cb7330cf87b476205fe
madihamehroj65@gmail.com <youremail@example.com>
2025-12-31 14:08:42 +0530

    MONGODB URI change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -34,7 +34,7 @@
 import axios from 'axios';
 
 const API_URL =
-  process.env.REACT_APP_API_URL || 'https://crowdmailer.onrender.com';
+  process.env.REACT_APP_API_URL || 'https://crowdmailer.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
